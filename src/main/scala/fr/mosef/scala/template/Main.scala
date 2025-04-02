8a590b74ab44a0a64e1d4e5209345bc9d9184b1c
Tonin RIVORY <toninrvr@MacBook-Air-de-Tonin.local>
2025-04-02 17:19:12 +0200

    bye l'api

--- src/main/scala/fr/mosef/scala/template/Main.scala
+++ src/main/scala/fr/mosef/scala/template/Main.scala
@@ -22,29 +22,5 @@ object Main extends App {
       .enableHiveSupport()
       .getOrCreate()
 
-    // ⚠️ API publique : pas besoin de clé ici
-    val apiUrl = "https://api.gouv.fr/documentation/api-rappel-conso"
-
-    try {
-      val response = Source.fromURL(apiUrl).mkString
-      println("Réponse brute de l'API Base Carbone :")
-      println(response)
-
-//      val json = JSON.parseFull(response)
-//      #println("JSON parsé (simplifié) :")
-//      #println(json)
-
-      // TODO : pour parser dans un DataFrame, il faudrait utiliser un vrai parser JSON (comme `spark.read.json`)
-      // Exemple (si tu veux aller plus loin) :
-      import sparkSession.implicits._
-      val rdd = sparkSession.sparkContext.parallelize(Seq(response))
-      val df = sparkSession.read.json(rdd)
-      df.show(false)
-
-    } catch {
-      case e: Exception =>
-        println(s"Erreur lors de l'appel à l'API Base Carbone : ${e.getMessage}")
-    }
-
     sparkSession.stop()
   }
