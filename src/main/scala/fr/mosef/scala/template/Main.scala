5d8f1d04ba04fc289aee6c8643d332e146c0793a
Tonin RIVORY <toninrvr@MacBook-Air-de-Tonin.local>
2025-04-02 16:16:46 +0200

    coucou

--- src/main/scala/fr/mosef/scala/template/Main.scala
+++ src/main/scala/fr/mosef/scala/template/Main.scala
@@ -5,62 +5,19 @@ import fr.mosef.scala.template.processor.Processor
 import fr.mosef.scala.template.processor.impl.ProcessorImpl
 import fr.mosef.scala.template.reader.Reader
 import fr.mosef.scala.template.reader.impl.ReaderImpl
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 import fr.mosef.scala.template.writer.Writer
 import org.apache.spark.SparkConf
 import com.globalmentor.apache.hadoop.fs.BareLocalFileSystem
 import org.apache.hadoop.fs.FileSystem
 
-object Main extends App with Job {
-
-  val cliArgs = args
-  val MASTER_URL: String = try {
-    cliArgs(0)
-  } catch {
-    case e: java.lang.ArrayIndexOutOfBoundsException => "local[1]"
-  }
-  val SRC_PATH: String = try {
-    cliArgs(1)
-  } catch {
-    case e: java.lang.ArrayIndexOutOfBoundsException => {
-      print("No input defined")
-      sys.exit(1)
-    }
-  }
-  val DST_PATH: String = try {
-    cliArgs(2)
-  } catch {
-    case e: java.lang.ArrayIndexOutOfBoundsException => {
-      "./default/output-writer"
-    }
-  }
-
-  val conf = new SparkConf()
-  conf.set("spark.driver.memory", "64M")
-  conf.set("spark.testing.memory", "471859200")
+object Main extends App {
 
   val sparkSession = SparkSession
     .builder
-    .master(MASTER_URL)
-    .config(conf)
-    .appName("Scala Template")
+    .master("local[1]")
     .enableHiveSupport()
     .getOrCreate()
-  
-  sparkSession
-    .sparkContext
-    .hadoopConfiguration
-    .setClass("fs.file.impl",  classOf[BareLocalFileSystem], classOf[FileSystem])
 
-
-  val reader: Reader = new ReaderImpl(sparkSession)
-  val processor: Processor = new ProcessorImpl()
-  val writer: Writer = new Writer()
-  val src_path = SRC_PATH
-  val dst_path = DST_PATH
-
-  val inputDF: DataFrame = reader.read(src_path)
-  val processedDF: DataFrame = processor.process(inputDF)
-  writer.write(processedDF, "overwrite", dst_path)
-
+  print(sparkSession.sql("SELECT 'A'").show())
 }
