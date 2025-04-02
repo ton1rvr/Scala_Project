39b3c53f13afd207b540a3a216d0ce8ed47691e9
Tonin RIVORY <toninrvr@MacBook-Air-de-Tonin.local>
2025-04-02 17:03:59 +0200

    api gouv

--- src/main/scala/fr/mosef/scala/template/Main.scala
+++ src/main/scala/fr/mosef/scala/template/Main.scala
@@ -11,13 +11,40 @@ import org.apache.spark.SparkConf
 import com.globalmentor.apache.hadoop.fs.BareLocalFileSystem
 import org.apache.hadoop.fs.FileSystem
 
+import scala.io.Source
+
 object Main extends App {
 
-  val sparkSession = SparkSession
-    .builder
-    .master("local[1]")
-    .enableHiveSupport()
-    .getOrCreate()
+    val sparkSession = SparkSession
+      .builder()
+      .appName("BaseCarboneAPI")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
 
-  print(sparkSession.sql("SELECT 'A'").show())
-}
+    // ⚠️ API publique : pas besoin de clé ici
+    val apiUrl = "https://api.gouv.fr/documentation/api-rappel-conso"
+
+    try {
+      val response = Source.fromURL(apiUrl).mkString
+      println("Réponse brute de l'API Base Carbone :")
+      println(response)
+
+//      val json = JSON.parseFull(response)
+//      #println("JSON parsé (simplifié) :")
+//      #println(json)
+
+      // TODO : pour parser dans un DataFrame, il faudrait utiliser un vrai parser JSON (comme `spark.read.json`)
+      // Exemple (si tu veux aller plus loin) :
+      import sparkSession.implicits._
+      val rdd = sparkSession.sparkContext.parallelize(Seq(response))
+      val df = sparkSession.read.json(rdd)
+      df.show(false)
+
+    } catch {
+      case e: Exception =>
+        println(s"Erreur lors de l'appel à l'API Base Carbone : ${e.getMessage}")
+    }
+
+    sparkSession.stop()
+  }
